6e420f7796e52c523edad6e01c53d128950b8bbc
Tyler Swartz <tgswartz@gmail.com>
2014-11-05 06:30:51 -0800

    added js file

---
+++ camp.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	
+	console.log($.parseXML("http://api.amp.active.com/camping/campsites?contractCode=CO&parkId=50032&arvdate=05/15/2012&api_key=2chxq68efd4azrpygt5hh2qu"));
+
+});
