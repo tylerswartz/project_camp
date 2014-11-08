b22c247d45acb6cae1fddf05bdc79756bdb3adc1
Tyler Swartz <tgswartz@gmail.com>
2014-11-08 14:29:19 -0800

    figuring out api call

--- camp.js
+++ camp.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {
-	
-	// console.log($.parseXML("http://api.amp.active.com/camping/campsites?contractCode=CO&parkId=50032&arvdate=05/15/2012&api_key=2chxq68efd4azrpygt5hh2qu"));
+var camp;
 
+$(document).ready(function() {
+		
 	$("form").submit(function(){
 		console.log($(".depart-input").val());
 		console.log($(".form-nights-input").val());
+
+		$.get('http://api.amp.active.com/camping/campgrounds?pstate=CA&siteType=2003&arvdate=12/15/2014&lengthOfStay=2&api_key=2chxq68efd4azrpygt5hh2qu', function(xml) {
+			camp = $.xml2json(xml);
+			console.log(camp);
+		})
 
 		return false;
 	});
