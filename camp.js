d5e1e43f61c39e8bb0dab07666bafc37846e6cb3
Tyler Swartz <tgswartz@gmail.com>
2014-11-08 13:55:07 -0800

    console log form

--- camp.js
+++ camp.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	
-	console.log($.parseXML("http://api.amp.active.com/camping/campsites?contractCode=CO&parkId=50032&arvdate=05/15/2012&api_key=2chxq68efd4azrpygt5hh2qu"));
+	// console.log($.parseXML("http://api.amp.active.com/camping/campsites?contractCode=CO&parkId=50032&arvdate=05/15/2012&api_key=2chxq68efd4azrpygt5hh2qu"));
+
+	$("form").submit(function(){
+		console.log($(".depart-input").val());
+		console.log($(".form-nights-input").val());
+
+		return false;
+	});
 
 });
