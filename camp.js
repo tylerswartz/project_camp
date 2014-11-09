c4b0c4d7d32eb7bade74b312774a6b3550241d66
Tyler Swartz <tgswartz@gmail.com>
2014-11-08 16:54:54 -0800

    working on api

--- camp.js
+++ camp.js
@@ -6,10 +6,14 @@ $(document).ready(function() {
 		console.log($(".depart-input").val());
 		console.log($(".form-nights-input").val());
 
-		$.get('http://api.amp.active.com/camping/campgrounds?pstate=CA&siteType=2003&arvdate=12/15/2014&lengthOfStay=2&api_key=2chxq68efd4azrpygt5hh2qu', function(xml) {
-			camp = $.xml2json(xml);
-			console.log(camp);
-		})
+		$.get("http://api.amp.active.com/camping/campgrounds?pstate=CA&siteType=2003&arvdate=12/15/2014&lengthOfStay=2&api_key=6gxhb929yg7ez8u3cr9mj9gj", function(response) { 
+    	console.log(response);
+		});
+
+		// $.get('http://api.amp.active.com/camping/campgrounds?pstate=CA&siteType=2003&arvdate=12/15/2014&lengthOfStay=2&api_key=6gxhb929yg7ez8u3cr9mj9gj', function(xml) {
+		// 	camp = $.xml2json(xml);
+		// 	console.log(camp);
+		// })
 
 		return false;
 	});
